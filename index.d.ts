5c0540c6228df4088bf8577efe0e84a72df47e38
Spencer Bigum <spencer.bigum@gmail.com>
2019-05-04 11:40:41 -0400

    added indext.d.ts for TS definitions

---
+++ index.d.ts
@@ -0,0 +1,49 @@
+import React from 'react'
+
+declare module 'gatsby-background-image' {
+  interface IFixedObject {
+    width: number
+    height: number
+    src: string
+    srcSet: string
+    base64?: string
+    tracedSVG?: string
+    srcWebp?: string
+    srcSetWebp?: string
+  }
+
+  interface IFluidObject {
+    aspectRatio: number
+    src: string
+    srcSet: string
+    sizes: string
+    base64?: string
+    tracedSVG?: string
+    srcWebp?: string
+    srcSetWebp?: string
+  }
+
+  interface IBackgroundImageProps {
+    resolutions?: IFixedObject,
+    sizes?: IFluidObject,
+    fixed?: IFixedObject,
+    fluid?: IFluidObject,
+    fadeIn?: string | boolean,
+    durationFadeIn?: number,
+    title?: string,
+    alt?: string
+    className?: string | object, // Support Glamor's css prop?.
+    critical?: boolean,
+    crossOrigin?: string | boolean,
+    style?: object | [], // Using PropTypes from RN. (not 100% sure of array type here)
+    backgroundColor?: string | boolean,
+    onLoad?: () => void
+    onStartLoad?: (param: { wasCached: boolean }) => void
+    onError?: (event: any) => void
+    Tag: string,
+    classId?: string,
+  }
+
+  export default class BackgroundImage extends React.Component<IBackgroundImageProps> {
+  }
+}
