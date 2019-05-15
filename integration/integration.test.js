08dd47365c95693a84e6f0e5a699583112dcfccb
Tim Hagn <mail@timhagn.com>
2019-05-15 22:13:52 +0200

    create simple integration test

---
+++ integration/integration.test.js
@@ -0,0 +1,23 @@
+/**
+ * @jest-environment node
+ */
+const GBI = require('gatsby-background-image').default
+const GBIES5 = require('gatsby-background-image-es5').default
+
+describe(`<BackgroundImage /> in gatsby-background-image-es5`, () => {
+  it(`should create new BackgroundImage instance`, () => {
+    const component = new GBIES5({
+      fadeIn: true,
+    })
+    expect(component).toMatchSnapshot()
+  })
+})
+
+describe(`<BackgroundImage /> in gatsby-background-image`, () => {
+  it(`should create new BackgroundImage instance`, () => {
+    const component = new GBI({
+      fadeIn: true,
+    })
+    expect(component).toMatchSnapshot()
+  })
+})
