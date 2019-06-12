c99c55bcf43b5db398687156b6aa334246292e16
Tim Hagn <mail@timhagn.com>
2019-06-12 21:55:30 +0200

    prettify gbi index.js with publish of v0.7.1

--- packages/gatsby-background-image/src/index.js
+++ packages/gatsby-background-image/src/index.js
@@ -1,10 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import getBackgroundStyles from './BackgroundUtils'
-import {
-  convertProps,
-  stripRemainingProps,
-} from './HelperUtils'
+import { convertProps, stripRemainingProps } from './HelperUtils'
 import {
   activateCacheForImage,
   activatePictureRef,
