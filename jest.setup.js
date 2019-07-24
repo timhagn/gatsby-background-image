e8dee95f6e6d4addec711e8997c2a744c7ff1cc6
Tim Hagn <mail@timhagn.com>
2019-07-24 20:09:33 +0200

    bump deps, change jest-dom to @testing-library/jest-dom

--- jest.setup.js
+++ jest.setup.js
@@ -1,7 +1,7 @@
 // jest.setup.js
 
 // add some helpful assertions
-import 'jest-dom/extend-expect'
+import '@testing-library/jest-dom/extend-expect'
 
 // this is basically: afterEach(cleanup)
 import '@testing-library/react/cleanup-after-each'
