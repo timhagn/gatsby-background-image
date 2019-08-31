bcff904d1f9b5f521ed8ae93c301830aa04ffa93
Tim Hagn <mail@timhagn.com>
2019-08-31 16:27:50 +0200

    prettify gatsby-node.js

--- packages/gatsby-background-image/src/gatsby-node.js
+++ packages/gatsby-background-image/src/gatsby-node.js
@@ -1,13 +1,13 @@
-import { isString }from './HelperUtils'
+import { isString } from './HelperUtils'
 
 exports.onCreateWebpackConfig = ({ plugins, actions }, { specialChars }) => {
- if (isString(specialChars)) {
-   actions.setWebpackConfig({
-     plugins: [
-       plugins.define({
-         __GBI_SPECIAL_CHARS__: JSON.stringify(specialChars),
-       }),
-     ],
-   });
- }
-};
+  if (isString(specialChars)) {
+    actions.setWebpackConfig({
+      plugins: [
+        plugins.define({
+          __GBI_SPECIAL_CHARS__: JSON.stringify(specialChars),
+        }),
+      ],
+    })
+  }
+}
